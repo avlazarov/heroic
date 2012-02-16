5c69f7749c15db897241d7fc2295cb13641728ef
Angel Lazarov <avlazarov@gmail.com>
2012-02-16 21:49:05 +0200

    change @player to @account.player at #destroy

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,10 +1,10 @@
 class ItemsController < ApplicationController
-  before_filter :require_user, :load_player
+  before_filter :require_user, :load_account
 
   # DELETE /items/1
   def destroy
     @item = Item.find params[:id]
-    if @item and @item.belongs_to? @player
+    if @item and @item.belongs_to? @account.player
       @item.destroy
       redirect_to inventory_path, notice: "Item '#{@item.name}' successfully removed!"
     else
