b47b34cc8fb2ac03ef3f7f759ad8395e8e535c7a
Angel Lazarov <avlazarov@gmail.com>
2012-02-16 17:23:41 +0200

    cosmetic changes

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -3,24 +3,24 @@ require 'spec_helper'
 describe Inventory do
   let(:inventory) { FactoryGirl.create :inventory }
 
-  it { should have_many(:items) }
+  it { should have_many(:items).dependent(:destroy) }
   it { should belong_to(:player) }
 
-  it 'Has one item after creation' do
+  it 'has one item after creation' do
     inventory.items.should have(1).item
   end
   
-  it 'Does not allow overflow with items' do
+  it 'does not allow overflow with items' do
     inventory.stub :capacity => 1
     expect { inventory.add_item FactoryGirl.build(:item) }.to raise_error
   end
 
-  it 'Does not allow invalid items to be added' do
-    expect { inventory.add_item FactoryGirl.build(:item) }.to raise_error
+  it 'does not allow invalid items to be added' do
+    expect { inventory.add_item Item.new }.to raise_error
   end
 
-  it 'Successfully adds valid item' do
-    item = FactoryGirl.build :item, name: 'Colossus Blade'
+  it 'successfully adds valid item' do
+    item = FactoryGirl.build :item
     expect { inventory.add_item item }.not_to raise_error
     item.save
     inventory.reload
