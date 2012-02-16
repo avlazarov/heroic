81b15760816d1f5bc992b5f002d362a3d07ffc36
Angel Lazarov <avlazarov@gmail.com>
2012-02-16 23:11:37 +0200

    add complete tests

--- spec/controllers/player_classes_controller_spec.rb
+++ spec/controllers/player_classes_controller_spec.rb
@@ -1,5 +1,21 @@
 require 'spec_helper'
 
 describe PlayerClassesController do
+  describe 'GET index' do
+    let(:player_classes) { [double] }
 
+    before do
+      PlayerClass.stub :all => player_classes
+    end
+
+    it 'initializes player_classes with all player classes' do
+      PlayerClass.should_receive(:all).and_return(player_classes)
+      get :index
+    end
+
+    it 'assigns player_classes to @player_classes' do
+      get :index
+      assigns(:player_classes).should == player_classes
+    end
+  end
 end
