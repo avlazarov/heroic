315508d06f97ff1bd6578c63f5f96650c000bf85
Angel Lazarov <avlazarov@gmail.com>
2012-02-16 17:36:06 +0200

    add dependency :destroy for players

--- spec/models/player_class_spec.rb
+++ spec/models/player_class_spec.rb
@@ -1,7 +1,7 @@
 require 'spec_helper'
 
 describe PlayerClass do
-  it { should have_many(:players) }
+  it { should have_many(:players).dependent(:destroy) }
   it { should validate_presence_of(:name) }
   it { should validate_numericality_of(:defense) }
   it { should validate_numericality_of(:attack) }
