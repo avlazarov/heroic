e2efe44d54c9020d1f3c5fd788f465cbb0dac8a2
Angel Lazarov <avlazarov@gmail.com>
2012-02-18 17:10:36 +0200

    add name validations

--- spec/models/player_class_spec.rb
+++ spec/models/player_class_spec.rb
@@ -1,3 +1,5 @@
+# encoding: UTF-8
+
 require 'spec_helper'
 
 describe PlayerClass do
@@ -6,4 +8,14 @@ describe PlayerClass do
   it { should validate_numericality_of(:defense) }
   it { should validate_numericality_of(:attack) }
   it { should validate_numericality_of(:experience_bonus) }
+
+  it { should validate_format_of(:name).not_with('1CLass').with_message('only letters allowed') }
+  it { should validate_format_of(:name).with('Class').with_message('only letters allowed') }
+
+  it { should_not allow_value('1Class').for(:name) }
+  it { should_not allow_value('Class1').for(:name) }
+  it { should_not allow_value('').for(:name) }
+  it { should_not allow_value('Cool Class').for(:name) }
+  it { should allow_value('barbarian').for(:name) }
+  it { should allow_value('Варварин').for(:name) }
 end
