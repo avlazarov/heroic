63d0bbf3bb169c03ff87bf018534204ee32daf81
Angel Lazarov <avlazarov@gmail.com>
2012-02-18 17:02:51 +0200

    add format of name validation

--- app/models/player_class.rb
+++ app/models/player_class.rb
@@ -1,7 +1,10 @@
 class PlayerClass < ActiveRecord::Base
   has_many :players, :dependent => :destroy
 
-  validates :name, :uniqueness => true, :presence => true
+  validates :name, :uniqueness => true,
+                   :presence => true, 
+                   :format => { :with => /\A[[:alpha:]]+\z/, 
+                                :message => 'only letters allowed' }
   validates_numericality_of :defense
   validates_numericality_of :attack
   validates_numericality_of :experience_bonus
