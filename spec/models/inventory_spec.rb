2e757a0f9d8d1ee3bbe9ad70ff62249c523c4fa4
Angel Lazarov <avlazarov@gmail.com>
2012-02-24 22:12:09 +0200

    add test for available_space method

--- spec/models/inventory_spec.rb
+++ spec/models/inventory_spec.rb
@@ -10,21 +10,33 @@ describe Inventory do
     inventory.items.should have(1).item
   end
   
-  it 'does not allow overflow with items' do
-    inventory.stub :capacity => 1
-    expect { inventory.add_item FactoryGirl.build(:item) }.to raise_error
+  describe '#add_item' do 
+    it 'does not allow overflow with items' do
+      inventory.stub :capacity => 1
+      expect { inventory.add_item FactoryGirl.build(:item) }.to raise_error
+    end
+
+    it 'does not allow invalid items to be added' do
+      expect { inventory.add_item Item.new }.to raise_error
+    end
+
+    it 'successfully adds valid item' do
+      item = FactoryGirl.build :item
+      expect { inventory.add_item item }.not_to raise_error
+      item.save
+      inventory.reload
+      inventory.items.should include(item)
+      inventory.items.should have(2).item # including initial
+    end
   end
 
-  it 'does not allow invalid items to be added' do
-    expect { inventory.add_item Item.new }.to raise_error
-  end
+  describe '#available_space' do
+    before do
+      inventory.stub :items => [double, double]
+    end
 
-  it 'successfully adds valid item' do
-    item = FactoryGirl.build :item
-    expect { inventory.add_item item }.not_to raise_error
-    item.save
-    inventory.reload
-    inventory.items.should include(item)
-    inventory.items.should have(2).item # including initial
+    it 'returns accurate available space' do
+      inventory.available_space.should == inventory.capacity - 2
+    end
   end
 end
